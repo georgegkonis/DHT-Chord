997e2a30b953bb7169db42e9df1885057079ef47
George Gkonis <53620284+georgegkonis@users.noreply.github.com>
2022-02-20 05:22:52 +0200

    Commit

--- src/dht_chord.py
+++ src/dht_chord.py
@@ -6,8 +6,10 @@ import pandas as pd
 FILE_PATH = '../files/test_data.xlsx'
 SHEET_NAME = 'Data'
 
-KEY_BITS = 32  # The number of bits in a key.
-CHORD_NODES = 8  # The number of nodes in the chord.
+KEY_BITS = 32                               # The number of bits in a key.
+CHORD_NODES = 8                             # The number of nodes in the chord.
+CHORD_SPACE = 2 ** KEY_BITS                 # The total space in the chord.
+NODE_SPACE = CHORD_SPACE // CHORD_NODES     # The total space in each node.
 
 
 class ChordRing:
@@ -22,11 +24,9 @@ class ChordRing:
         self.create_nodes()
 
     def create_nodes(self):
-        total_space = 2 ** KEY_BITS
-        node_space = total_space // CHORD_NODES
         prev_node = None
 
-        for key in range(node_space - 1, total_space, node_space):
+        for key in range(NODE_SPACE - 1, CHORD_SPACE, NODE_SPACE):
             new_node = Node(key)
 
             if prev_node is not None:
@@ -37,8 +37,8 @@ class ChordRing:
             prev_node = new_node
 
         self.total_nodes = len(self.node_dict)
-        self.first_node = self.node_dict[node_space - 1]
-        self.last_node = self.node_dict[total_space - 1]
+        self.first_node = self.node_dict[NODE_SPACE - 1]
+        self.last_node = self.node_dict[CHORD_SPACE - 1]
 
         self.first_node.predecessor = self.last_node
         self.last_node.successor = self.first_node
@@ -53,22 +53,23 @@ class ChordRing:
                 i *= 2
 
     def load_data(self):
+        # Load the data from the file.
         node = self.first_node
         df = pd.read_excel(FILE_PATH, SHEET_NAME)
         np = df.to_numpy()
         key_list = []
 
+        # Hash the keys and add the data to the nodes.
         for line in np:
-            # Hash the key.
             key = str(line[0]).encode('utf-8')
             key = hashlib.sha1(key).hexdigest()
             key = int(key, 16) % 2 ** KEY_BITS
-            key_list.append(key)
             print(hex(key))
             successor = node.find_successor(key)
             successor.data[key] = line[0]
 
-        # Check for duplicates
+            key_list.append(key)
+
         key_set = set(key_list)
         print('\nHas Duplicates:', len(key_list) != len(key_set))
 
